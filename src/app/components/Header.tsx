f68ff8fdba052e5a0d514eb68553b9a7672d53ef
Rodinchuk <rodandser@gmail.com>
2025-04-27 22:20:59 +0300

    create deployment

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,8 @@
 "use client";
-import React, { useState } from 'react';
-import styles from '@/app/components/Header.module.css';
-import Image from 'next/image'; 
-import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import styles from "@/app/components/Header.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -24,52 +24,83 @@ const Header = () => {
   const scrollToHome = () => {
     document.getElementById("home")?.scrollIntoView({ behavior: "smooth" });
   };
-  const router = useRouter();
-
- 
 
   return (
     <header className={styles.header}>
-      <div className={styles.headerContainer}><a href='#' className={styles.logoContainer}>
-        <Image
-          onClick={scrollToHome}
-          src="/images/logoWhite.png" 
-          alt="Logo"
-          width={50}
-          height={50} 
-          className={styles.logo}
-          onMouseEnter={(e) => e.currentTarget.classList.add(styles.spin)}
-          onMouseLeave={(e) => e.currentTarget.classList.remove(styles.spin)}
-        />
-      </a>
-      <nav className={styles.nav}>
-        <ul className={styles.navList}>
-            <li><a href="/" style={{ fontWeight: '900' }}>Про подію</a></li>
+      <div className={styles.headerContainer}>
+        <div className={styles.logoContainer}>
+          <Image
+            onClick={scrollToHome}
+            src="/images/logoWhite.png"
+            alt="Logo"
+            width={50}
+            height={50}
+            className={styles.logo}
+            onMouseEnter={(e) => e.currentTarget.classList.add(styles.spin)}
+            onMouseLeave={(e) => e.currentTarget.classList.remove(styles.spin)}
+          />
+        </div>
+        <nav className={styles.nav}>
+          <ul className={styles.navList}>
+            <li>
+              <Link href="/" style={{ fontWeight: "900" }}>Про подію</Link>
+            </li>
+            <li>
+              <Link href="/schedule">
+                Розклад
+              </Link>
+            </li>
+            <li>
+              <Link href="/speakers">Спікери</Link>
+            </li>
+          </ul>
+        </nav>
+        <button onClick={scrollToPartik} className={`${styles.partnerButton} ${styles.desktopOnly}`}>
+          Стати учасником
+        </button>
+        <button onClick={scrollToPartnership} className={`${styles.partnerButton1} ${styles.desktopOnly}`}>
+          Стати партнером
+        </button>
 
-          <li><a href="/schedule">Розклад</a></li>
-          <li><a href="/speakers">Спікери</a></li>
-        </ul>
-      </nav>
-      <a onClick={scrollToPartik} className={`${styles.partnerButton} ${styles.desktopOnly}`}>Стати учасником</a>
-      <a onClick={scrollToPartnership} className={`${styles.partnerButton1} ${styles.desktopOnly}`}>Стати партнером</a>
-
-
-      
-      <div className={styles.menuIcon} onClick={toggleMenu}>
-        <div className={styles.bar}></div>
-        <div className={styles.bar}></div>
-        <div className={styles.bar}></div>
+        <div className={styles.menuIcon} onClick={toggleMenu}>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+        </div>
+        <div className={`${styles.mobileMenu} ${isMenuOpen ? styles.open : ""}`}>
+          <button className={styles.closeButton} onClick={closeMenu}>
+            ✕
+          </button>
+          <ul>
+            <li>
+              <Link href="/"  style={{ fontWeight: "900" }} onClick={closeMenu}>
+                Про подію
+              </Link>
+            </li>
+            <li>
+              <Link href="/schedule"  onClick={closeMenu}>
+                Розклад
+              </Link>
+            </li>
+            <li>
+              <Link href="/speakers" onClick={closeMenu}>
+                Контакти
+              </Link>
+            </li>
+            <li>
+              <button className={styles.partnerButton} onClick={closeMenu}>
+                <Link href="/#bot">Стати учасником</Link>
+              </button>
+            </li>
+            <li>
+              <button className={styles.partnerButton1} onClick={closeMenu}>
+                <Link href="/#partnership">Стати партнером
+                </Link>
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className={`${styles.mobileMenu} ${isMenuOpen ? styles.open : ''}`}>
-        <button className={styles.closeButton} onClick={closeMenu}>✕</button>
-        <ul>
-          <li><a href="/"style={{ fontWeight: '900' }} onClick={closeMenu}>Про подію</a></li>
-          <li><a href="/schedule" onClick={closeMenu}>Розклад</a></li>
-          <li><a href="/speakers" onClick={closeMenu}>Контакти</a></li>
-          <li><a href="/#bot"><button className={styles.partnerButton} onClick={closeMenu}>Стати учасником</button></a></li>
-          <li><a href="/#partnership"><button className={styles.partnerButton1} onClick={closeMenu}>Стати партнером</button></a></li>
-        </ul>
-      </div></div>
     </header>
   );
 };
